b823ad780e9d9b3e882964b42b23f7c8705ddcc0
Craig_Donavin <crdonavin@gmail.com>
2020-04-20 19:22:41 -0500

    adds equipment reducer

---
+++ src/reducers/manageEquipment.js
@@ -0,0 +1,23 @@
+function manageEquipment(state = { 
+    equipment: [],
+    requesting: false }, action) {
+        switch (action.type) {
+
+            case 'REQUESTING_EQUIPMENT':
+                return {
+                    ...state,
+                    equipment: [...state.equipment],
+                    requesting: true
+                }
+            
+            case 'ADD_EQUIPMENT':
+                return {
+                    ...state,
+                    equipment: action.equipment,
+                    requesting: false
+                }
+            
+            default:
+                return state
+        }
+    }
