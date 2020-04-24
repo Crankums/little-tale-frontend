ce2fe360a584dae5e66bee438c4cc64c29266271
Craig_Donavin <crdonavin@gmail.com>
2020-04-23 22:26:17 -0500

    adds text input to PostInput

--- src/components/posts/PostInput.js
+++ src/components/posts/PostInput.js
@@ -5,7 +5,11 @@ export class PostInput extends Component{
     render(){
         return(
             <div>
+                <form>
                 Inputs go here!
+                <input type='text'></input>
+                <input type='submit'></input>
+                </form>
             </div>
         )
     }
