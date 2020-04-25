1411bee79f2e5b183e307bf52c822444fd71f9c6
Craig_Donavin <crdonavin@gmail.com>
2020-04-25 00:04:27 -0500

    increaseIndex() refactor

--- src/actions/promptActions.js
+++ src/actions/promptActions.js
@@ -1,18 +1,7 @@
 
 export function increaseIndex(props){
-    if (props.persons.index < props.persons.persons.length-1) {
-        props.persons.index+=1
-    } else {
-        props.persons.index = 0
-    }
-    if (props.monsters.index < props.monsters.monsters.length-1) {
-        props.monsters.index+=1
-    } else {
-        props.monsters.index = 0
-    }
-    if (props.equipment.index < props.equipment.equipment.length-1) {
-        props.equipment.index+=1
-    } else {
-        props.equipment.index = 0
-    }
+    props.persons.index < props.persons.persons.length-1 ? props.persons.index+=1 : props.persons.index = 0
+    props.monsters.index < props.monsters.monsters.length-1 ? props.monsters.index+=1 : props.monsters.index = 0
+    props.equipment.index < props.equipment.equipment.length-1 ? props.equipment.index+=1 : props.equipment.index = 0
+    
 }
