e46b48c3601147c0f147284f87df83c98114d3c0
Craig_Donavin <crdonavin@gmail.com>
2020-04-25 00:37:49 -0500

    adds code for PostInput, styles text box

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,8 +1,13 @@
 /*
 Create Post
-
+    -fetch POST request - are routes for this written yet?
 Delete Post
-
+    -fetch DELETE request
+Upvote Post
+Downvote Post
+React to Post
+    -fetch UPDATE(PUT) request
+    
 Admin:
 Update posts
 Lock replies*/
--- src/components/posts/PostInput.js
+++ src/components/posts/PostInput.js
@@ -2,12 +2,36 @@ import React, { Component } from "react";
 
 export class PostInput extends Component{
 
+    state = {
+        text: ''
+    }
+
+    handleOnChange(event){
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    handleOnSubmit(event) {
+        event.preventDefault()
+        this.setState({
+            text: ''
+        })
+    }
+
     render(){
         return(
             <div>
-                <form>
-                <textarea></textarea>
-                <input type='submit' placeholder="inputs go here"></input>
+                <form onSubmit = {(event) => this.handleOnSubmit(event)}>
+                    <br></br>
+                    <textarea  placeholder="inputs go here"
+                    value = {this.state.text}
+                    onChange={(event)=> this.handleOnChange(event)}
+                    style={{
+                        width: 400,
+                        height: 200}}/>
+                    <br></br>
+                    <input type='submit' />
                 </form>
             </div>
         )
