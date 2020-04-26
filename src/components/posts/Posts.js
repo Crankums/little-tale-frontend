8fc72efba8eb62d4bb683808cfe2d25635000474
Craig_Donavin <crdonavin@gmail.com>
2020-04-26 18:00:14 -0500

    removes console.log from posts

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -7,7 +7,6 @@ export class Posts extends Component{
     render(){
         
         const posts = this.props.posts.posts
-        console.log(this.props)
         const postsList = posts.map(post => <Post post={post} deletePost={this.props.deletePost}/>)
         
         return(
