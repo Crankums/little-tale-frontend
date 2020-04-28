873d6415398307d84a9ab2585655bce356946cca
Craig_Donavin <crdonavin@gmail.com>
2020-04-28 18:57:20 -0500

    adds some routes, removes unused code from App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux'
 import PromptsContainer from './containers/PromptsContainer';
-// import { fetchMonsters } from './actions/monsterActions'
-// import { fetchPersons } from './actions/personActions'
-// import { fetchEquipment } from './actions/equipmentActions'
+
 import PostsContainer from './containers/PostsContainer';
 import { getCurrentUser } from './actions/currentUserActions';
 import NavBar from './components/NavBar';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Login from './components/Login';
 import Signup from './components/Signup';
 
@@ -16,9 +14,7 @@ class App extends Component {
 
   
   componentDidMount(){
-    // this.props.fetchMonsters()
-    // this.props.fetchPersons()
-    // this.props.fetchEquipment()
+
     this.props.getCurrentUser()
     
   }
@@ -29,23 +25,18 @@ class App extends Component {
           <NavBar />
           <PromptsContainer />
           <PostsContainer />
+        <Switch>
           <Route exact path='/login' component={Login}/>
-          <Route exact patch='/signup' component={Signup} />
+          <Route exact path='/signup' component={Signup} />
           <Route /> {/* <--this on will be for profile */}
           <Route /> {/* <--show all posts */}
+        </Switch>
         </div>
     )
   }
 }
 
 
-// const mapStateToProps = state => {
-//   return {
-//     monsters: state.monsters,
-//     persons: state.persons,
-//     equipment: state.equipment,
-//     currentUser: state.currentUser
-//   }
-// }
+
 
 export default connect(null, { getCurrentUser })(App);
