1bc84b6c9a022576e554f8172fb71d708921f9a3
Craig_Donavin <crdonavin@gmail.com>
2020-04-30 22:59:00 -0500

    implemented some basic routing

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/login">{loggedIn ? <Redirect to="/"/> : <Login />}</Route>
           <Route exact path="/signup">{loggedIn ? <Redirect to="/"/> : <Signup />}</Route>
-    <Route exact path='/posts'> {loggedIn ? <PostsContainer /> : <Redirect to='/'/>}</Route>
+          <Route exact path='/posts'> {loggedIn ? <PostsContainer /> : <Redirect to='/'/>}</Route>
           <Route exact path='/about'>{loggedIn ? <About />: <Redirect to="/"/>  }</Route>
           <Route /> {/* <--show all posts */}
         </Switch>
