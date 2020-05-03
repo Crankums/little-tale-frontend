ba5bfb73a0321ada41f65829f3d93803489eed1a
Craig_Donavin <crdonavin@gmail.com>
2020-05-02 19:03:15 -0500

    creates update form outline

---
+++ src/components/posts/EditPost.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export const EditPost = ({posts, updatePosts}) => {
+
+    return(
+        <div>
+            <form>
+                <textarea />
+                <input type='submit'>Submit</input>
+                
+            </form>
+        </div>
+    )
+
+}
+
+export default EditPost
